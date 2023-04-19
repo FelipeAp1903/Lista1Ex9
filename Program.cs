0b27d52303e1bcd35d8fabce0b700f500a172f1e
FelipeAp1903 <131301360+FelipeAp1903@users.noreply.github.com>
2023-04-19 17:32:08 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double raio;
+            double diametro;
+            double area;
+
+            Console.Write("coloque o valor do diâmetro: ");
+            diametro = double.Parse(Console.ReadLine());
+            raio = diametro / 2;
+            area = Math.PI * Math.Pow(raio, 2);
+            Console.WriteLine("o valor da área é: {0}", area);
+        }
+    }
+}
